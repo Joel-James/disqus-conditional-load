df9b7234922d8327c58315339b11e2f31a56e110
Joel James <joel@incsub.com>
2018-03-23 18:43:38 +0530

    Added i18n

---
+++ includes/class-dcl-i18n.php
@@ -0,0 +1,31 @@
+<?php
+
+// If this file is called directly, abort.
+defined( 'ABSPATH' ) or exit;
+
+/**
+ * DCL internationalization class.
+ *
+ * A class definition that sets textdomain for the plugin to make it compatible
+ * with translations.
+ *
+ * @category   Core
+ * @package    DCL
+ * @subpackage i18n
+ * @author     Joel James <mail@cjoel.com>
+ * @license    http://www.gnu.org/licenses/ GNU General Public License
+ * @link       https://dclwp.com
+ */
+class DCL_i18n  {
+	/**
+	 * Set the textdomain for the plugin.
+	 *
+	 * @since  2.0.0
+	 * @access public
+	 *
+	 * @return void.
+	 */
+	public function set_textdomain() {
+		load_plugin_textdomain( DCL_DOMAIN, false, DCL_DIR . '/languages/' );
+	}
+}
