88b4a6cf8181ad0836c12740fef1113553932244
Joel James <me@joelsays.com>
2018-08-12 20:11:42 +0530

    Fix translation strings

--- admin/views/admin-display.php
+++ admin/views/admin-display.php
@@ -23,15 +23,15 @@ defined( 'ABSPATH' ) || die( 'K. Bye.' );
 	<h2><?php printf( __( 'Disqus Conditional Load <span class="subtitle">by <a href="%s" target="_blank" title="Developed by Joel James">Joel James</a> ( v%d )</span>', 'disqus-conditional-load' ), 'https://duckdev.com', DCL_VERSION ); ?></h2>
 	<?php if ( isset( $_GET['settings-updated'] ) ) : ?>
 		<div class="notice notice-success is-dismissible">
-			<p><strong><?php _e( 'DCL settings updated successfully', 'disqus-conditional-load' ); ?></strong></p>
+			<p><strong><?php esc_html_e( 'DCL settings updated successfully', 'disqus-conditional-load' ); ?></strong></p>
 		</div>
 	<?php endif; ?>
 	<ul class="ui-tabs-nav">
 		<li class="<?php echo 'general' === $current_tab ? 'ui-state-active' : ''; ?>">
-			<a href="?page=dcl-settings" class="tab-premium"><?php _e( 'DCL Settings', 'disqus-conditional-load' ); ?><span class="newred_dot">&bull;</span></a>
+			<a href="?page=dcl-settings" class="tab-premium"><?php esc_html_e( 'DCL Settings', 'disqus-conditional-load' ); ?><span class="newred_dot">&bull;</span></a>
 		</li>
 		<li class="<?php echo 'pro' === $current_tab ? 'ui-state-active' : ''; ?>">
-			<a href="?page=dcl-settings&tab=pro" class="tab-premium"><?php _e( 'DCL Pro & Info', 'disqus-conditional-load' ); ?><span class="newred_dot">&bull;</span></a>
+			<a href="?page=dcl-settings&tab=pro" class="tab-premium"><?php esc_html_e( 'DCL Pro & Info', 'disqus-conditional-load' ); ?><span class="newred_dot">&bull;</span></a>
 		</li>
 	</ul>
 
--- admin/views/admin-general.php
+++ admin/views/admin-general.php
@@ -16,132 +16,130 @@ defined( 'ABSPATH' ) || die( 'K. Bye.' );
  * @link       https://dclwp.com
  */
 ?>
-<h3><?php _e( 'Conditional Load Settings', 'disqus-conditional-load' ); ?></h3>
+<h3><?php esc_html_e( 'Conditional Load Settings', 'disqus-conditional-load' ); ?></h3>
 <form method="post" action="options.php">
 	<?php settings_fields( 'dcl_gnrl_options' ); ?>
 	<table class="form-table">
 		<tbody>
 		<tr valign="top">
 			<th scope="row">
-				<label><?php _e( 'How to Load Disqus', 'disqus-conditional-load' ); ?><br>
-					<span
-						class="description thin"><?php _e( 'to improve performance', 'disqus-conditional-load' ); ?></span>
+				<label><?php esc_html_e( 'How to Load Disqus', 'disqus-conditional-load' ); ?><br>
+					<span class="description thin"><?php esc_html_e( 'to improve performance', 'disqus-conditional-load' ); ?></span>
 				</label>
 			</th>
 			<td>
 				<select id="dcl_type" class="select" typle="select" name="dcl_gnrl_options[dcl_type]" required>
-					<option value="click" <?php selected( $options['dcl_type'], 'click' ); ?>><?php _e( 'On Click', 'disqus-conditional-load' ); ?></option>
-					<option value="scroll" <?php selected( $options['dcl_type'], 'scroll' ); ?>><?php _e( 'On Scroll', 'disqus-conditional-load' ); ?></option>
-					<option value="normal" <?php selected( $options['dcl_type'], 'normal' ); ?>><?php _e( 'Normal (no lazy load)', 'disqus-conditional-load' ); ?></option>
-					<option disabled><?php _e( 'On Scroll Start (Pro only)', 'disqus-conditional-load' ); ?></option>
+					<option value="click" <?php selected( $options['dcl_type'], 'click' ); ?>><?php esc_html_e( 'On Click', 'disqus-conditional-load' ); ?></option>
+					<option value="scroll" <?php selected( $options['dcl_type'], 'scroll' ); ?>><?php esc_html_e( 'On Scroll', 'disqus-conditional-load' ); ?></option>
+					<option value="normal" <?php selected( $options['dcl_type'], 'normal' ); ?>><?php esc_html_e( 'Normal (no lazy load)', 'disqus-conditional-load' ); ?></option>
+					<option disabled><?php esc_html_e( 'On Scroll Start (Pro only)', 'disqus-conditional-load' ); ?></option>
 				</select>
 				<br/>
-				<span
-					class="description thin"><?php _e( 'This feature will let you prevent Disqus from automatically loading comments and scripts on pages, posts or whatever it is. If you choose "Normal" comments will be loaded normally and no lazy load effect will be there. Also please note that, with "Click" option, we will add a very small inline style to adjust the button alignment.', 'disqus-conditional-load' ); ?></span>
+				<span class="description thin"><?php esc_html_e( 'This feature will let you prevent Disqus from automatically loading comments and scripts on pages, posts or whatever it is. If you choose "Normal" comments will be loaded normally and no lazy load effect will be there. Also please note that, with "Click" option, we will add a very small inline style to adjust the button alignment.', 'disqus-conditional-load' ); ?></span>
 			</td>
 		</tr>
 
 		<tr valign="top">
 			<th scope="row">
-				<label><?php _e( 'Disqus Comments Width', 'disqus-conditional-load' ); ?><br>
-					<span class="description thin"><?php _e( 'to match your theme', 'disqus-conditional-load' ); ?></span>
+				<label><?php esc_html_e( 'Disqus Comments Width', 'disqus-conditional-load' ); ?><br>
+					<span class="description thin"><?php esc_html_e( 'to match your theme', 'disqus-conditional-load' ); ?></span>
 				</label>
 			</th>
 			<td>
 				<input placeholder="Default" type="number" name="dcl_gnrl_options[dcl_div_width]" value="<?php echo $options['dcl_div_width']; ?>" size="20">
 				<input type="radio" name="dcl_gnrl_options[dcl_div_width_type]" <?php checked( $options['dcl_div_width_type'], '%' ); ?> value="%">%
 				<input type="radio" name="dcl_gnrl_options[dcl_div_width_type]" <?php checked( $options['dcl_div_width_type'], 'px' ); ?> value="px">px<br/>
-				<span class="description thin"><?php _e( 'If Disqus comments are not looking good because of the large or smaller width, you can adjust the width of comments division here. Just enter the width size. Leave empty if not required. Please note that we will add a very small inline CSS to adjust this width if you enable this.', 'disqus-conditional-load' ); ?></span>
+				<span class="description thin"><?php esc_html_e( 'If Disqus comments are not looking good because of the large or smaller width, you can adjust the width of comments division here. Just enter the width size. Leave empty if not required. Please note that we will add a very small inline CSS to adjust this width if you enable this.', 'disqus-conditional-load' ); ?></span>
 			</td>
 		</tr>
 
 		<tr valign="top">
 			<th scope="row">
-				<label><?php _e( 'Count Script', 'disqus-conditional-load' ); ?><br>
-					<span class="description thin"><?php _e( 'only load if needed', 'disqus-conditional-load' ); ?></span>
+				<label><?php esc_html_e( 'Count Script', 'disqus-conditional-load' ); ?><br>
+					<span class="description thin"><?php esc_html_e( 'only load if needed', 'disqus-conditional-load' ); ?></span>
 				</label>
 			</th>
 			<td>
 				<select id="dcl_count_disable" class="select" typle="select" name="dcl_gnrl_options[dcl_count_disable]">
-					<option value="1" <?php selected( $options['dcl_count_disable'], 1 ); ?>>Enable</option>
-					<option value="0" <?php selected( $options['dcl_count_disable'], 0 ); ?>>Disable</option>
+					<option value="1" <?php selected( $options['dcl_count_disable'], 1 ); ?>><?php esc_html_e( 'Enable', 'disqus-conditional-load' ); ?></option>
+					<option value="0" <?php selected( $options['dcl_count_disable'], 0 ); ?>><?php esc_html_e( 'Disable', 'disqus-conditional-load' ); ?></option>
 				</select>
 				<br/>
-				<span class="description thin"><?php _e( 'By default Disqus may load a script (count.js) to get the comments count to show somewhere on your pages. Disabling this feature can improve your page loading speed but may conflict with some themes.', 'disqus-conditional-load' ); ?></span>
+				<span class="description thin"><?php esc_html_e( 'By default Disqus may load a script (count.js) to get the comments count to show somewhere on your pages. Disabling this feature can improve your page loading speed but may conflict with some themes.', 'disqus-conditional-load' ); ?></span>
 			</td>
 		</tr>
 		<tr valign="top">
 			<th scope="row">
-				<label><?php _e( 'Button Text', 'disqus-conditional-load' ); ?><br>
-					<span class="description thin"><?php _e( 'to attract visitors', 'disqus-conditional-load' ); ?></span>
+				<label><?php esc_html_e( 'Button Text', 'disqus-conditional-load' ); ?><br>
+					<span class="description thin"><?php esc_html_e( 'to attract visitors', 'disqus-conditional-load' ); ?></span>
 				</label>
 			</th>
 			<td>
 				<input placeholder="Load Comments" type="text" name="dcl_gnrl_options[dcl_btn_txt]" value="<?php echo $options['dcl_btn_txt']; ?>" size="20"><br/>
-				<span class="description thin"><?php _e( 'If you are loading Disqus using On Click feature, then there will be a button to load comments. If you want to use custom text on the button, enter it here. By default it will be "Load Comments" (Works only with "On Click" option).', 'disqus-conditional-load' ); ?></span>
+				<span class="description thin"><?php esc_html_e( 'If you are loading Disqus using On Click feature, then there will be a button to load comments. If you want to use custom text on the button, enter it here. By default it will be "Load Comments" (Works only with "On Click" option).', 'disqus-conditional-load' ); ?></span>
 			</td>
 		</tr>
 		<tr valign="top">
 			<th scope="row">
-				<label><?php _e( 'Custom Button Class', 'disqus-conditional-load' ); ?><br>
-					<span class="description thin"><?php _e( 'to customize the button', 'disqus-conditional-load' ); ?></span>
+				<label><?php esc_html_e( 'Custom Button Class', 'disqus-conditional-load' ); ?><br>
+					<span class="description thin"><?php esc_html_e( 'to customize the button', 'disqus-conditional-load' ); ?></span>
 				</label>
 			</th>
 			<td>
 				<input type="text" name="dcl_gnrl_options[dcl_btn_class]" value="<?php echo $options['dcl_btn_class']; ?>" size="20"><br/>
-				<span class="description thin"><?php _e( 'Here you can add any custom class to the button. By using custom class you can use your own style for comment button. Leave empty if you do not care. (Works only with "On Click" option)', 'disqus-conditional-load' ); ?></span>
+				<span class="description thin"><?php esc_html_e( 'Here you can add any custom class to the button. By using custom class you can use your own style for comment button. Leave empty if you do not care. (Works only with "On Click" option)', 'disqus-conditional-load' ); ?></span>
 			</td>
 		</tr>
 		<tr valign="top">
 			<th scope="row">
-				<label><?php _e( 'Loading Message', 'disqus-conditional-load' ); ?><br>
-					<span class="description thin"><?php _e( 'to impress visitors', 'disqus-conditional-load' ); ?></span>
+				<label><?php esc_html_e( 'Loading Message', 'disqus-conditional-load' ); ?><br>
+					<span class="description thin"><?php esc_html_e( 'to impress visitors', 'disqus-conditional-load' ); ?></span>
 				</label>
 			</th>
 			<td>
-				<input placeholder="<?php _e( 'Loading..', 'disqus-conditional-load' ); ?>" type="text" name="dcl_gnrl_options[dcl_message]" value="<?php echo $options['dcl_message']; ?>" size="20"><br/>
-				<span class="description thin"><?php _e( 'There may be few milliseconds delay before Disqus starts loading when visitors clicks on the button. Enter something here, and we will show that during that delay. By Default it will be "Loading..". You may use html tags like <code>&lt;p&gt; &lt;b&gt; &lt;h2&gt;</code>(Works only with "On Click" option)', 'disqus-conditional-load' ); ?></span>
+				<input placeholder="<?php esc_html_e( 'Loading..', 'disqus-conditional-load' ); ?>" type="text" name="dcl_gnrl_options[dcl_message]" value="<?php echo $options['dcl_message']; ?>" size="20"><br/>
+				<span class="description thin"><?php esc_html_e( 'There may be few milliseconds delay before Disqus starts loading when visitors clicks on the button. Enter something here, and we will show that during that delay. By Default it will be "Loading..". You may use html tags like <p><b><h2> (Works only with "On Click" option)', 'disqus-conditional-load' ); ?></span>
 			</td>
 		</tr>
 		<tr valign="top">
 			<th scope="row">
-				<label><?php _e( 'Disable on CPTs', 'disqus-conditional-load' ); ?><br>
-					<span class="description thin"><?php _e( 'to skip custom post types', 'disqus-conditional-load' ); ?></span>
+				<label><?php esc_html_e( 'Disable on CPTs', 'disqus-conditional-load' ); ?><br>
+					<span class="description thin"><?php esc_html_e( 'to skip custom post types', 'disqus-conditional-load' ); ?></span>
 				</label>
 			</th>
 			<td>
 				<input placeholder="product" type="text" name="dcl_gnrl_options[dcl_cpt_exclude]" value="<?php echo $options['dcl_cpt_exclude']; ?>"><br/><br/>
-				<span class="description thin"><?php _e( 'If Disqus adds its scripts on custom post types, and you dont like it, enter the custom post type here. Separate with comma.', 'disqus-conditional-load' ); ?></span>
+				<span class="description thin"><?php esc_html_e( 'If Disqus adds its scripts on custom post types, and you dont like it, enter the custom post type here. Separate with comma.', 'disqus-conditional-load' ); ?></span>
 			</td>
 		</tr>
 		<tr valign="top">
 			<th scope="row">
-				<label><?php _e( 'Caching Support', 'disqus-conditional-load' ); ?><br>
-					<span class="description thin"><?php _e( 'to work with caching plugins', 'disqus-conditional-load' ); ?></span>
+				<label><?php esc_html_e( 'Caching Support', 'disqus-conditional-load' ); ?><br>
+					<span class="description thin"><?php esc_html_e( 'to work with caching plugins', 'disqus-conditional-load' ); ?></span>
 				</label>
 			</th>
 			<td>
 				<select id="dcl_caching" class="select" name="dcl_gnrl_options[dcl_caching]">
-					<option value="1" <?php selected( $options['dcl_caching'], 1 ); ?>><?php _e( 'Enable', 'disqus-conditional-load' ); ?></option>
-					<option value="0" <?php selected( $options['dcl_caching'], 0 ); ?>><?php _e( 'Disable', 'disqus-conditional-load' ); ?></option>
+					<option value="1" <?php selected( $options['dcl_caching'], 1 ); ?>><?php esc_html_e( 'Enable', 'disqus-conditional-load' ); ?></option>
+					<option value="0" <?php selected( $options['dcl_caching'], 0 ); ?>><?php esc_html_e( 'Disable', 'disqus-conditional-load' ); ?></option>
 				</select>
 				<br/>
-				<span class="description thin">If you are having issues loading Disqus comments when caching is enabled, please enable this.</span>
+				<span class="description thin"><?php esc_html_e( 'If you are having issues loading Disqus comments when caching is enabled, please enable this.', 'disqus-conditional-load' ); ?></span>
 			</td>
 		</tr>
 		<tr valign="top">
 			<th scope="row">
-				<label><?php _e( 'Rocker Loader Support', 'disqus-conditional-load' ); ?><br>
-					<span class="description thin">to ignore the Disqus script</span>
+				<label><?php esc_html_e( 'Rocker Loader Support', 'disqus-conditional-load' ); ?><br>
+					<span class="description thin"><?php esc_html_e( 'to ignore the Disqus script', 'disqus-conditional-load' ); ?></span>
 				</label>
 			</th>
 			<td>
 				<select id="dcl_cfasync" class="select" name="dcl_gnrl_options[dcl_cfasync]">
-					<option value="1" <?php selected( $options['dcl_cfasync'], 1 ); ?>><?php _e( 'Enable', 'disqus-conditional-load' ); ?></option>
-					<option value="0" <?php selected( $options['dcl_cfasync'], 0 ); ?>><?php _e( 'Disable', 'disqus-conditional-load' ); ?></option>
+					<option value="1" <?php selected( $options['dcl_cfasync'], 1 ); ?>><?php esc_html_e( 'Enable', 'disqus-conditional-load' ); ?></option>
+					<option value="0" <?php selected( $options['dcl_cfasync'], 0 ); ?>><?php esc_html_e( 'Disable', 'disqus-conditional-load' ); ?></option>
 				</select>
 				<br/>
-				<span class="description thin"><?php _e( 'If you are using Cloudflare Rocker Loader, enable this to have <a href="https://support.cloudflare.com/hc/en-us/articles/200169436-How-can-I-have-Rocket-Loader-ignore-my-script-s-in-Automatic-Mode-" target="_blank">Rocket Loader ignore</a> the Disqus scripts by adding the <code>data-cfasync="false"</code> attribute to the script tag.', 'disqus-conditional-load' ); ?></span>
+				<span class="description thin"><?php sprintf( __( 'If you are using Cloudflare Rocker Loader, enable this to have %1$sRocket Loader ignore%2$s the Disqus scripts by adding the %3$sdata-cfasync="false"%4$s attribute to the script tag.', 'disqus-conditional-load' ), '<a href="https://support.cloudflare.com/hc/en-us/articles/200169436-How-can-I-have-Rocket-Loader-ignore-my-script-s-in-Automatic-Mode-" target="_blank">', '</a>', '<code>', '</code>' ); ?></span>
 			</td>
 		</tr>
 		</tbody>
